a6cc7abdc9941e4f137a415adfc9927deb75fdf0
thanhtrnnn <thanhbx6a@gmail.com>
2025-11-27 00:37:20 +0700

    snippet netbeans fix

--- snippet-netbeans.java
+++ snippet-netbeans.java
@@ -7,9 +7,8 @@ import java.util.*;
 
 public class ${name} {
     public static void main(String[] args) throws IOException {
-        String pkgPath = "src" + File.separator + ${name}.class.getPackageName().replace(".", File.separator);
-        File inputFile = new File(pkgPath + File.separator + "input.txt");
-        File outputFile = new File(pkgPath + File.separator + "output.txt");
+        File inputFile = new File("input.txt");
+        File outputFile = new File("output.txt");
         if (inputFile.exists()) {
             System.setIn(new FileInputStream(inputFile));
             System.setOut(new PrintStream(outputFile));
